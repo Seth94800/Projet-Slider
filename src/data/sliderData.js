a018e511219924dafa305c5f3bd6730d4ce143d3
Seth Gaara <83479322+Seth94800@users.noreply.github.com>
2023-07-30 01:04:19 +0200

    Envoie sur GitHub

---
+++ src/data/sliderData.js
@@ -0,0 +1,23 @@
+ const sliderData = [
+  {
+    id: 1,
+    description: "Elephants"
+  },
+  {
+    id: 2,
+    description: "Girafes"
+  },
+  {
+    id: 3,
+    description: "Lionne"
+  },
+  {
+    id: 4,
+    description: "Phacochere"
+  },
+  {
+    id: 5,
+    description: "Zebre"
+  },
+]
+export default sliderData;
